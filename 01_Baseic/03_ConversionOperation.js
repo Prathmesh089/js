1fa96f96d9f5ff5a25e83957bdba7f335f3362b4
Prathmesh089 <patilprathmesh087@gmail.com>
2025-01-22 10:43:14 +0000

    Conversion of datatypes

--- 01_Baseic/03_ConversionOperation.js
+++ 01_Baseic/03_ConversionOperation.js
@@ -1,13 +1,19 @@
 let score = "33abc"
 
-console.log(typeof score)
-console.log(typeof (score))
+// console.log(typeof score)
+// console.log(typeof (score))
 
  let valueInNumber = Number(score)
- console.log(typeof valueInNumber)
- console.log(valueInNumber)
+//  console.log(typeof valueInNumber)
+//  console.log(valueInNumber)
 
  let isLoggedIn = 0
 
  let booleanIsLoggedIn = Boolean(isLoggedIn)
- console.log(booleanIsLoggedIn)
+//  console.log(booleanIsLoggedIn)
+
+ //**********************Operations*****************************/
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
