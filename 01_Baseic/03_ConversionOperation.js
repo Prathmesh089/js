4d696e540dc697f7adf0c1b1fbf633093122f6f4
Prathmesh089 <patilprathmesh087@gmail.com>
2025-01-27 16:59:24 +0000

    String

--- 01_Baseic/03_ConversionOperation.js
+++ 01_Baseic/03_ConversionOperation.js
@@ -17,3 +17,15 @@ let score = "33abc"
 let value = 3
 let negValue = -value
 // console.log(negValue)
+
+let userOne ={
+    email:"user@google.com",
+    upi: "user@bly"
+}
+
+let user2 = userOne
+
+user2.email = "prathm@gmail.com"
+
+console.log(userOne.email)
+console.log(user2.email)
---
+++ 01_Baseic/05_String.js
@@ -0,0 +1,14 @@
+const name = "Prathmesh"
+const repoCount = 2
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+const gameName = new String('prathmesh')
+
+// console.log(gameName[0])
+// console.log(gameName.__proto__)
+
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(3)) 
+// console.log(gameName.indexOf('t'))
